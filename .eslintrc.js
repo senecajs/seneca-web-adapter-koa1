cc2952d728e55f979c169793d8326eaf5efdece0
EamonnK <amen_cionna@hotmail.com>
2017-12-24 15:56:01 +0000

    Code quality control updated - .eslintrc.js added/prettier installed

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: 'eslint:recommended',
+  env: {
+    node: true,
+    es6: true
+  },
+  "parserOptions": {
+    "ecmaVersion": 6
+  },
+  rules: {
+    'no-console': 0,
+    'require-yield': 0
+  }
+}
